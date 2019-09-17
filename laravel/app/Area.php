80f74867280678c4df8c0c905a5e69c407059b96
johnatan <johnatangto@gmail.com>
2019-09-17 20:14:46 -0300

    Sprint 1 finalizada

--- laravel/app/Area.php
+++ laravel/app/Area.php
@@ -4,12 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class  Area extends Model
-{
-  public function condominio(){
+class Area extends Model {
+
+    public function condominio() {
         return $this->belongsTo('\App\Condominio');
-   }
-   public function unidade(){
-        return $this->belongsTo('\App\Unidade');
-   }
+    }
+
 }
