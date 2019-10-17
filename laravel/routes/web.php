6d06f90079e87bdca00207ed778ac097790efe92
johnatan <johnatangto@gmail.com>
2019-10-16 21:17:08 -0300

    segunda sprint finalizada

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,20 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'area'], function(){
+Route::get('listar', 'AreaController@listar');
+Route::get('criar', 'AreaController@criar');
+Route::get('{id/editar', 'AreaController@editar');
+Route::get('{id}/remover', 'AreaController@remover');
+Route::get('salvar', 'AreaController@salvar');
+});
